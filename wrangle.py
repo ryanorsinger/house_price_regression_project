c304d3258a6f84a63a02ff8beb65df02c0d1f947
RyanOrsinger <ryan.orsinger@gmail.com>
2019-10-17 16:57:39 -0500

    Add todos for prepare

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,14 @@
 from acquire import get_data
 from prep import prep_data
 
-df = get_data()
-df = prep_data(df)
+raw = get_data()
+df = prep_data(raw)
+
+# TO DOs
+
+# 1. plot distributions of individual variables to identify outliers
+#        beds vs. baths, baths vs. sqft, sqft vs, price, etc...
+
+# 2. Add a data dictionary that defines all fields used in model or analysis
+#        with explanation of why that field? Why one field over another, etc...
 
